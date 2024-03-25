525152ea967129165062f5d1e831a815153411c9
henriquePinheiro <48884506+HenriqueMartinelliPinheiro@users.noreply.github.com>
2024-03-25 19:22:12 -0300

    Update client.py

--- Atividade02/client.py
+++ Atividade02/client.py
@@ -4,25 +4,21 @@ import socket
 HOST = '127.0.0.1'
 PORT = 12345
 
-# Criação do socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
 
-# Recebendo e respondendo às questões
-while True:
-    question = client_socket.recv(1024).decode()
-    if not question:
-        break
-    
+total_questoes = int(client_socket.recv(1024).decode())
+print(f"Serão feitas {total_questoes} perguntas.")
+
+for i in range(total_questoes):
+    question = client_socket.recv(2048).decode()
     print(question)
-    print(client_socket.recv(1024).decode())
     print("Digite a resposta: ")
     answer = input()
     client_socket.send(answer.encode())
 
 # Recebendo feedback
-feedback = client_socket.recv(1024).decode()
-print("teste")
+feedback = client_socket.recv(2048).decode()
 print(feedback)
 
 # Fechamento da conexão
