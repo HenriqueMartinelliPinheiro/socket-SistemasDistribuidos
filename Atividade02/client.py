64e6d6f4b0a35821608cac346cf2ed5c83c50ccc
henriquePinheiro <48884506+HenriqueMartinelliPinheiro@users.noreply.github.com>
2024-03-27 08:07:27 -0300

    Update client.py

--- Atividade02/client.py
+++ Atividade02/client.py
@@ -1,14 +1,13 @@
 import socket
 
-# Configurações do cliente
 HOST = '127.0.0.1'
-PORT = 12345
+PORT = 8080
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
 
 total_questoes = int(client_socket.recv(1024).decode())
-print(f"Serão feitas {total_questoes} perguntas.")
+print("Serão feitas ",total_questoes," perguntas.")
 
 for i in range(total_questoes):
     question = client_socket.recv(2048).decode()
@@ -17,9 +16,7 @@ for i in range(total_questoes):
     answer = input()
     client_socket.send(answer.encode())
 
-# Recebendo feedback
 feedback = client_socket.recv(2048).decode()
 print(feedback)
 
-# Fechamento da conexão
 client_socket.close()
